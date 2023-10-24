8ff949e6f05501f94ad928e859fa8b611cdbefdb
the-chronicles <oluseungideon@gmail.com>
2023-10-24 21:55:31 +0100

    Finished part one of the movie site

--- src/index.js
+++ src/index.js
@@ -3,33 +3,36 @@ import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
 
-import StartRating from "./StarRating";
+// import StartRating from "./StarRating";
+import TextExpander from "./TextExpander";
 
-function Test() {
-  const [movieRating, setMovieRating] = useState(0);
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0);
 
-  return (
-    <div>
-      <StartRating
-        color="blue"
-        maxRating={10}
-        onSetMovieRating={setMovieRating}
-      />
-      <p>This movie was rated {movieRating} stars</p>
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <StartRating
+//         color="blue"
+//         maxRating={10}
+//         onSetMovieRating={setMovieRating}
+//       />
+//       <p>This movie was rated {movieRating} stars</p>
+//     </div>
+//   );
+// }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating
+    {/* <StartRating
       maxRating={5}
       messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
     />
     <StartRating size={24} color="red" className="test" defaultRating={3} />
 
-    <Test />
+    <Test /> */}
+
+    <TextExpander />
   </React.StrictMode>
 );
