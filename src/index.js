5a13384bf99c850c21144624f287f1f36f5f0b9e
the-chronicles <oluseungideon@gmail.com>
2023-10-24 15:26:18 +0100

    Reused the star components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
 
-import StartRating from './StarRating';
+import StartRating from "./StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StartRating
+        color="blue"
+        maxRating={10}
+        onSetMovieRating={setMovieRating}
+      />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StartRating maxRating={5}/>
+    <StartRating
+      maxRating={5}
+      messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+    />
+    <StartRating size={24} color="red" className="test" defaultRating={3} />
 
+    <Test />
   </React.StrictMode>
 );
